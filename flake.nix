1fae064cbaffa8947df723eb0e67325d1ae95cf6
unixpariah <100892812+unixpariah@users.noreply.github.com>
2025-09-22 22:32:51 +0200

    init
    Change-Id: I6a6a69644f593c3254e6a2e4e596c0a308ceea52

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs.tooling.url = "github:mox-desktop/tooling";
+
+  outputs =
+    { self, tooling, ... }:
+    tooling.lib.mkMoxFlake {
+      devShells = tooling.lib.forAllSystems (pkgs: {
+        default = pkgs.mkShell (
+          pkgs.lib.fix (finalAttrs: {
+            buildInputs = builtins.attrValues {
+              inherit (pkgs)
+                rustToolchain
+                rust-analyzer-unwrapped
+                nixd
+                wayland
+                ;
+            };
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath finalAttrs.buildInputs;
+            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+          })
+        );
+      });
+
+      packages = tooling.lib.forAllSystems (pkgs: {
+        wl-clicker-rs = pkgs.callPackage ./nix/package.nix {
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = pkgs.rustToolchain;
+            rustc = pkgs.rustToolchain;
+          };
+        };
+        default = self.packages.${pkgs.stdenv.hostPlatform.system}.wl-clicker-rs;
+      });
+
+      nixosModules = {
+        wl-clicker-rs = import ./nix/nixos.nix;
+        default = self.nixosModules.wl-clicker-rs;
+      };
+    };
+}
